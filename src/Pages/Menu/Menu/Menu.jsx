95a7da8747d3032fd1b19188086f176f333f43b0
ahasan7861 <ahfahasan786@gmail.com>
2023-05-25 08:44:48 +0600

    menuCategory added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,22 +1,57 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuCategory from "./MenuCategory";
 import Cover from "../../Shared/Cover/Cover";
 
-
 const Menu = () => {
+  const [menu] = useMenu();
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
+
       <Cover
         img={"https://i.ibb.co/Kzr1ymS/banner3.jpg"}
         title={"our menu"}
       ></Cover>
+
+
+      {/* main cover */}
+
+      <SectionTitle
+        subHeading="Don't Miss"
+        heading="Today's Offer"
+      ></SectionTitle>
+
+        {/* offered menu items */}
+
+      <MenuCategory items={offered}></MenuCategory>
+
       
+          {/* dessert menu items */}
+
+        <MenuCategory items={desserts} title="Dessert" img="https://i.ibb.co/hmv2bkK/dessert-bg.jpg"></MenuCategory>
+
+        <MenuCategory items={pizza} title="Pizza" img="https://i.ibb.co/dr0cbJf/pizza-bg.jpg"></MenuCategory>
+
+        <MenuCategory items={salad} title="Salad" img="https://i.ibb.co/sJpPnJ5/salad-bg.jpg"></MenuCategory>
+
+        <MenuCategory items={soup} title="Soup" img="https://i.ibb.co/fFnXnXt/soup-bg.jpg"></MenuCategory>
+
       
-      
+
+        
+
     </div>
   );
 };
--- src/Pages/Menu/Menu/MenuCategory.jsx
+++ src/Pages/Menu/Menu/MenuCategory.jsx
@@ -1,12 +1,23 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React from "react";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+import Cover from "../../Shared/Cover/Cover";
 
-const MenuCategory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const MenuCategory = ({ items, title, img }) => {
+  return (
+    <div className="pt-8">
+     { title && <Cover
+        img={img}
+        title={title}
+      ></Cover> }
+
+      <div className="grid md:grid-cols-2 gap-10 my-20">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuCategory;

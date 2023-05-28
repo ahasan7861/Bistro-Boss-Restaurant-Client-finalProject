bd8ba71003a9481d497526baa9a840e24ae1f273
ahasan7861 <ahfahasan786@gmail.com>
2023-05-28 19:24:54 +0600

    something addded

--- src/hooks/UseCart.jsx
+++ src/hooks/UseCart.jsx
@@ -1,5 +1,22 @@
-const UseCart = email => {
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProviders";
 
-}
+const UseCart = () => {
+  const { user } = useContext(AuthContext);
+
+  const { isLoading, data: cart = [] } = useQuery({
+    queryKey: ["carts", user?.email],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/carts?email=${user?.email}`
+      );
+      return res.json();
+    },
+  });
+  return [cart, isLoading]
+
+
+};
 
 export default UseCart;

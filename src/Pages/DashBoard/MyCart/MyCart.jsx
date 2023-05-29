f21a0d02ee85664c5525df8d4ed404dd0c868963
ahasan7861 <ahfahasan786@gmail.com>
2023-05-29 11:38:12 +0600

    Table added

--- src/Pages/DashBoard/MyCart/MyCart.jsx
+++ src/Pages/DashBoard/MyCart/MyCart.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import UseCart from "../../../hooks/UseCart";
+import { FaTrashAlt } from "react-icons/fa";
 
 const MyCart = () => {
     const [cart] = UseCart();
@@ -11,11 +12,65 @@ const MyCart = () => {
       <Helmet>
         <title>Bistro Boss | My Cart</title>
       </Helmet>
-      <div className="uppercase">
+      <div className="uppercase font-semibold flex justify-evenly mb-6 h-[60px] items-center">
         <h3 className="text-3xl">Total Items: {cart.length}</h3>
         <h3 className="text-3xl">Total Price: ${total}</h3>
         <button className="btn btn-warning btn-sm text-white">PAY</button>
       </div>
+
+      <div className="overflow-x-auto w-full">
+  <table className="table w-full">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>
+          <label>
+            #
+          </label>
+        </th>
+        <th>Food</th>
+        <th>Item Name</th>
+        <th>Price</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        cart.map((item, index) => <tr
+        key={item._id}>
+            <td>
+              {index + 1}
+            </td>
+            <td>
+              
+                <div className="avatar">
+                  <div className="mask mask-squircle w-12 h-12">
+                    <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                  </div>
+                </div>
+                
+              
+            </td>
+            <td>
+              {item.name}
+            </td>
+            <td className="text-end">${item.price}</td>
+            <td>
+              <button className="btn btn-ghost btn-sm bg-red-600 text-white"><FaTrashAlt></FaTrashAlt> </button>
+            </td>
+          </tr>)
+      }
+      
+      
+    </tbody>
+    
+    
+    
+  </table>
+</div>
+
+
+
     </div>
   );
 };

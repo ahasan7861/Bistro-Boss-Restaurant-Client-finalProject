8f571f3610051233320f5daa1ab6c77b0e51626e
ahasan7861 <ahfahasan786@gmail.com>
2023-05-31 11:19:58 +0600

    Jwt set addded

--- src/hooks/UseCart.jsx
+++ src/hooks/UseCart.jsx
@@ -5,11 +5,15 @@ import { AuthContext } from "../Providers/AuthProviders";
 const UseCart = () => {
   const { user } = useContext(AuthContext);
 
+  const token = localStorage.getItem('access-token')
+
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["carts", user?.email],
     queryFn: async () => {
       const res = await fetch(
-        `https://bistro-boss-server-ahfahasan786-gmailcom.vercel.app/carts?email=${user?.email}`
+        `https://bistro-boss-server-ahfahasan786-gmailcom.vercel.app/carts?email=${user?.email}`,{headers: {
+          authorization: `bearer ${token}`
+        }} 
       );
       return res.json();
     },
